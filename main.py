e54b413460210d9d8df050a1039b4abc7412c433
Ishan Sharma <ishansharma1320@gmail.com>
2025-10-02 16:58:44 -0700

    added health checks and exposed ports

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ def get_translation(request: dict):
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
 
+@app.get("/")
+def health_check():
+    return "PONG"
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
